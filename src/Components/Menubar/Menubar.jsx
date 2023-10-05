4acfb98e9ec4b7dd4e72c6a08cd308b45660ae49
rjspyk5 <rjspyk5@gmail.com>
2023-10-05 18:43:39 +0600

    Coditional msz notification Option

--- src/Components/Menubar/Menubar.jsx
+++ src/Components/Menubar/Menubar.jsx
@@ -8,11 +8,9 @@ import { FaFacebookMessenger } from 'react-icons/fa';
 import './Menubar.css';
 export default function Menubar() {
     const [totalmsz, settotalmsz] = useState([]);
-
     useEffect(() => {
         settotalmsz(getDataFromLocalStorage());
     }, []);
-
     return (
         <div className="sticky-top">
             <Navbar expand="lg" bg="dark" data-bs-theme="dark">
@@ -24,18 +22,18 @@ export default function Menubar() {
                             <Nav.Link>
                                 <NavLink className="text-decoration-none text-light d-flex " to={'/message'}>
                                     <FaFacebookMessenger className="MesssengerFullLogoStyle" style={{ marginTop: '-4px' }} size={35}></FaFacebookMessenger>
-                                    <div className="MessengerFloatingMessege">
-                                        <h6> {totalmsz.length}</h6>
-                                    </div>
+                                    {totalmsz.length !== 0 && (
+                                        <div className="MessengerFloatingMessege">
+                                            <h6> {totalmsz.length}</h6>
+                                        </div>
+                                    )}
                                 </NavLink>
                             </Nav.Link>
-
                             <Nav.Link>
                                 <NavLink className="text-decoration-none text-light " to={'/user'}>
                                     All User
                                 </NavLink>
                             </Nav.Link>
-
                             <Nav.Link>
                                 <NavLink className="text-decoration-none text-light " to={'/signin'}>
                                     Sign In
