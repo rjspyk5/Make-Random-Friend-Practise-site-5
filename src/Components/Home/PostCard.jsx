6676586e828c8ad908979c4e4f7184bac4286d0d
rjspyk5 <rjspyk5@gmail.com>
2023-10-07 14:32:51 +0600

    Like button functionality added

--- src/Components/Home/PostCard.jsx
+++ src/Components/Home/PostCard.jsx
@@ -1,27 +1,46 @@
-import React, { useContext } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
 import Card from 'react-bootstrap/Card';
+import './postCard.css';
+import { SlLike } from 'react-icons/sl';
 
 export const PostCard = (props) => {
     const { title, body, id } = props.post;
+
     const data = props.data;
     const EachPerson = data && data.find((el) => el.serialnumber === id);
+
     const pic = EachPerson && EachPerson.picture.medium;
 
     const fullname = EachPerson && `${EachPerson.name.title} ${EachPerson.name.first} ${EachPerson.name.last}`;
+    const [like, setlike] = useState(false);
+
+    const handleClick = () => {
+        setlike((pre) => !pre);
+    };
+
+    let liked;
+    like ? (liked = { color: 'red' }) : (liked = { color: 'black' });
 
     return (
         <div className="m-3">
             <Card>
                 <Card.Body>
                     <Card.Title className="d-flex">
-                        <img className=" rounded-circle " src={pic} alt="" />
+                        <div>
+                            <img className=" rounded-circle img-fluid" src={pic} alt="" />
+                        </div>
 
                         <p className="pt-4 ps-2">{fullname}</p>
                     </Card.Title>
                     <Card.Text>{body}</Card.Text>
-                    <Button className="me-2">Like</Button>
-                    <Button>Comment</Button>
+                    <hr />
+                    <div className="d-flex">
+                        <button className="me-2 LikeButtonStyle w-50 " onClick={handleClick}>
+                            <SlLike style={liked} className="like" />
+                            <span style={liked}>Like</span>
+                        </button>
+                        <button className="LikeButtonStyle w-50">Comment</button>
+                    </div>
                 </Card.Body>
             </Card>
         </div>
